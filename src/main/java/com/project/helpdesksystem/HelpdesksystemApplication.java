83f7846edbe285af8438989670fed987b7f61405
ovini <Mario23junior>
2022-07-18 05:13:28 -0300

    Iniciando api de helpDesk

---
+++ src/main/java/com/project/helpdesksystem/HelpdesksystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.helpdesksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdesksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdesksystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/helpdesksystem/HelpdesksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.helpdesksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdesksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
