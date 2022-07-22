64edf8437ae9f41607f87910ebcfd78541ac7d43
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-07-22 04:43:16 -0300

    Remodelando estritura principal do projeto

--- src/main/java/com/project/helpdesksystem/repository/TecnicoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.helpdesksystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.helpdesksystem.domain.Tecnico;
-
-public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
-}
